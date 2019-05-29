2ae3cae14220bd2d1eba129268640f304b817527
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2019-05-29 21:26:24 +0200

    Activities CreateAccount, ForgotPassword and Created new Activities: ReisterAsDonor and RegisterAsVolunteer

---
+++ app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/ForgotPasswordActivity.java
@@ -0,0 +1,28 @@
+package gsoc.mihaela.claudia.diosan.gsoc_2019_python_library_for_real_time_data_visualization;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    Button btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        btnBack = (Button) findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/RegisterAsVolunteerActivity.java
@@ -0,0 +1,13 @@
+package gsoc.mihaela.claudia.diosan.gsoc_2019_python_library_for_real_time_data_visualization;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterAsVolunteerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_as_volunteer);
+    }
+}
