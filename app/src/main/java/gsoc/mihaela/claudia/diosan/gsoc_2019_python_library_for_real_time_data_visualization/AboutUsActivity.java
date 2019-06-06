104fe7cb6510719a7c63254ae703324590c9ffe5
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2019-06-06 23:52:14 +0200

    About Us activity complete

--- app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/AboutUsActivity.java
+++ app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/AboutUsActivity.java
@@ -1,13 +1,29 @@
 package gsoc.mihaela.claudia.diosan.gsoc_2019_python_library_for_real_time_data_visualization;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutUsActivity extends AppCompatActivity {
+
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        btnBack = (Button) findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(AboutUsActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
