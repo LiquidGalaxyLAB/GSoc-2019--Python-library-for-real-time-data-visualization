4a261e9a383f93f6f2d5fff011fd8fafd18bb97a
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2019-06-07 22:39:20 +0200

    StatisticsActivity

--- app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/StatisticsActivity.java
+++ app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/StatisticsActivity.java
@@ -1,13 +1,28 @@
 package gsoc.mihaela.claudia.diosan.gsoc_2019_python_library_for_real_time_data_visualization;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class StatisticsActivity extends AppCompatActivity {
+
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_statistics);
+
+        btnBack = (Button) findViewById(R.id.btn_back);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StatisticsActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
