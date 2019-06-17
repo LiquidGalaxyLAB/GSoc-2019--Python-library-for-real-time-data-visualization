e75d63636a4f9d48864a7e1580ce524bb8d81e7a
ClaudiaMihaelaDiosan <mcd1591@gmaile.com>
2019-06-18 00:27:21 +0200

    AboutUs Fragment for the navgation drawer menu

--- app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/HomeActivity.java
+++ app/src/main/java/gsoc/mihaela/claudia/diosan/gsoc_2019_python_library_for_real_time_data_visualization/HomeActivity.java
@@ -1,18 +1,22 @@
 package gsoc.mihaela.claudia.diosan.gsoc_2019_python_library_for_real_time_data_visualization;
 
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
     ActionBarDrawerToggle actionBarDrawerToggle;
+    Toolbar toolbar;
 
 
 
@@ -38,5 +42,32 @@ public class HomeActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
         }
+
+
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+
+        if (id == R.id.nav_help) {
+            // Handle the camera action
+        } else if (id == R.id.nav_about) {
+            toolbar.setTitle(R.string.title_nav_about);
+
+
+
+        } else if (id == R.id.nav_support) {
+            toolbar.setTitle(R.string.title_nav_support);
+
+
+        } else if (id == R.id.nav_logout) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 
 }
